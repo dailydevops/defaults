660ed857e4fc541abe0a2b3725415d9de8d8b352
Martin Stühmer <me@samtrion.net>
2024-11-18 22:43:10 +0100

    feat: Added `NetEvolve.Defaults.Analyzer` project
    with several diagnostics

---
+++ src/NetEvolve.Defaults.Analyzer/RuleCategory.cs
@@ -0,0 +1,14 @@
+﻿namespace NetEvolve.Defaults.Analyzer;
+
+/// <summary>
+/// Describes the available categories for diagnostics.
+/// </summary>
+internal enum RuleCategory : byte
+{
+    Design,
+    Naming,
+    Style,
+    Usage,
+    Performance,
+    Security,
+}
